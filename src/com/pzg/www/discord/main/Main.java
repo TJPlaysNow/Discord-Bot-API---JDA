efa4c764c0017b563224668f3d7d1a4bdef317b5
TJPlaysNow <pannebakertj@gmail.com>
2018-03-06 14:40:53 -0600

    Remove main stuff
    Left testing stuff in main method

--- src/com/pzg/www/discord/main/Main.java
+++ src/com/pzg/www/discord/main/Main.java
@@ -9,10 +9,5 @@ import com.pzg.www.discord.object.Bot;
 public class Main {
 	public static void main(String[] args) {
 //		TODO: Nothing.
-		Bot bot = new Bot("Mzc4MzY0NjY3MzUwNzQ1MDg4.DX-PkQ.G-R-QYK2s1JYealZDJDBdxiyzO4", "!");
-		bot.addCommand(new MuteCommand(bot));
-		bot.addCommand(new UnmuteCommand(bot));
-		bot.addCommand(new HelpCommand(bot));
-		bot.addConsoleCommand(new StopConsoleCommand(bot));
 	}
 }
