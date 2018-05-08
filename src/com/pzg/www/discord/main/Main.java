5a1c39922f0ff6b73ff5c2d8bde0c0ea113ffe61
TJPlaysNow <pannebakertj@gmail.com>
2018-05-08 00:10:37 -0500

    2.0 Rework
    Loads and loads of things have changed.

--- src/com/pzg/www/discord/main/Main.java
+++
@@ -1,7 +0,0 @@
-package com.pzg.www.discord.main;
-
-public class Main {
-	public static void main(String[] args) {
-//		TODO: Nothing.
-	}
-}
---
+++ src/com/tjplaysnow/discord/main/Main.java
@@ -0,0 +1,7 @@
+package com.tjplaysnow.discord.main;
+
+public class Main {
+	public static void main(String[] args) {
+//		TODO: Nothing.
+	}
+}
