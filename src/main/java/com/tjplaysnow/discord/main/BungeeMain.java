11b176d1e8a0b06aabe5827524395886df9dd28e
TJPlaysNow <pannebakertj@gmail.com>
2022-07-21 17:48:47 -0500

    Added Metrics to see how many people are using.

--- src/main/java/com/tjplaysnow/discord/main/BungeeMain.java
+++ src/main/java/com/tjplaysnow/discord/main/BungeeMain.java
@@ -1,10 +1,11 @@
 package com.tjplaysnow.discord.main;
 
+import com.tjplaysnow.discord.main.metrics.Bungee;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class BungeeMain extends Plugin {
 	@Override
 	public void onEnable() {
-//		TODO: Nothing.
+		Bungee metrics = new Bungee(this, 15872);
 	}
 }
--- src/main/java/com/tjplaysnow/discord/main/PluginMain.java
+++ src/main/java/com/tjplaysnow/discord/main/PluginMain.java
@@ -1,10 +1,11 @@
 package com.tjplaysnow.discord.main;
 
+import com.tjplaysnow.discord.main.metrics.Spigot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginMain extends JavaPlugin {
 	@Override
 	public void onEnable() {
-//		TODO: Nothing.
+		Spigot metrics = new Spigot(this, 15871);
 	}
 }
